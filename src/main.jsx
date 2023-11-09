fb2d26b3ba7b89ceea6a25c63727f72f63b3ed99
Shamimreza82 <shamimrezaone@gmail.com>
2023-11-09 23:06:23 +0600

    added footer section

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import router from './Routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl m-auto font-inter'>
     <RouterProvider router={router}></RouterProvider>
+    </div>
   </React.StrictMode>,
 )
